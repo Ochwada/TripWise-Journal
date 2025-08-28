01185fef5f295f3144b410b3928222e77873829b
Linda Ochwada <nakhulolinda@yahoo.com>
2025-08-28 11:57:29 +0200

    General setup

--- src/main/java/com/tripwise/TripJournal/TripJournalApplication.java
+++ src/main/java/com/tripwise/TripJournal/TripJournalApplication.java
@@ -1,13 +1,38 @@
 package com.tripwise.TripJournal;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TripJournalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TripJournalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TripJournalApplication.class, args);
+    }
+
+    static {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        String[] envVars = {
+                "PORT", "SPRING_APPLICATION_NAME",
+                "SPRING_DATA_MONGODB_URI", "OAUTH2_ISSUER_URI",
+                "JWT_AUDIENCE", "JWT_ALG", "MEDIA_BASE_URL",
+                "LOG_ROOT_LEVEL", "LOG_APP_LEVEL"
+        };
+
+        for (String key : envVars) {
+
+            String value = dotenv.get(key);
+
+            if (value != null) {
+                System.setProperty(key, value); // Makes it accessible via System.getProperty
+                System.out.println("✅ " + key + " loaded and set.");
+            } else {
+                System.out.println("⚠️" + key + " not found in env variables. Skipping System property.");
+            }
+        }
+    }
+
 
 }
